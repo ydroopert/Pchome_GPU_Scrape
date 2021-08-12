e6246eea38946ab9a40523f5a4f1050eb26ac8bc
Ting L <69746614+ydroopert@users.noreply.github.com>
2021-08-12 19:20:20 +0800

    Update index.js
    Fixed multi browser + not closing problem

--- index.js
+++ index.js
@@ -1,52 +1,69 @@
 import express from 'express';
 import puppeteer from 'puppeteer';
+import {RTX3090,RTX3080TI,RTX3080,RTX3070TI,RTX3070,RTX3060TI,RTX3060,RTX2060,GTX1660TI} from './url.js'
 const port = process.env.PORT || '5000';
 const app = express();
-let GList = [];
+let GList = []
 
-async function scrapeProduct(url){
-    
-    //puppeteer setup
-    const browser = await puppeteer.launch();
+async function scrapeProduct() {
+  try {
+    const array = []
+    const url = [RTX3090,RTX3080TI,RTX3080,RTX3070TI,RTX3070,RTX3060TI,RTX3060,RTX2060,GTX1660TI]
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ],
+    });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
-    await page.goto(url);
-    const textContent = await page.evaluate(() => {
-        
-        //getting element with Jquery then push object to array
-        
-        let List= []
-        const result = document.querySelectorAll("div.prod_info");
-        result.forEach(ele => {if(ele.querySelector('ul.price_box > li > span.site_btn.orderNow > button')!== null || ele.querySelector('ul.price_box > li > span.site_btn.add24hCart > button')!==null){
-            console.log(ele);
-                const GraphicCard = {
-                    'name': ele.querySelector('a').innerText,
-                    'price': ele.querySelector('.value').innerText,
-                    'link': ele.querySelector('a').href
-                }
-                List.push(GraphicCard);
+    process.once('unhandledRejection', () => {
+      browser.close();
+    });
+    for (let i = 0; i < url.length; i++) {
+      await page.goto(url[i])
+      const result = await page.evaluate(() => {
+        const List = []
+        allProduct = document.querySelectorAll("div.prod_info")
+        allProduct.forEach(ele => {
+          if (ele.querySelector('ul.price_box > li > span.site_btn.orderNow > button') !== null || ele.querySelector('ul.price_box > li > span.site_btn.add24hCart > button') !== null) {
+            const GraphicCard = {
+              'name': ele.querySelector('a').innerText,
+              'price': ele.querySelector('.value').innerText,
+              'link': ele.querySelector('a').href
             }
-        });
+            List.push(GraphicCard)
+          }
+        })
         return List;
-    })
-    browser.close();
-    return textContent;
-}
-async function createList() {
-    // import url here
-    
-    GList = [];
-    const product = (await scrapeProduct(url))
-
-    GList.push(product)
+      })
+      array.push(result)
+    }
+    await browser.close();
+    return array;
+  } catch (err) {
+    console.log(err);
+    await browser.close();
   }
-//auto update
-  setTimeout(() => { createList() }, 13000)
+}
 
-app.get('/',(req,res) => {
-    res.send(GList);
+async function createList(){
+  try {
+    GList = [];
+    GList.push(await scrapeProduct())
+  }catch (err) {
+    console.log(err);
+  }
+}
+
+createList()
+setInterval(()=>createList(),30000)
+
+app.get('/', (req, res) => {
+  res.send(GList);
 })
 
-app.listen(port, (req,res)=> {
-    console.log('server up')
+app.listen(port, (req, res) => {
+  console.log('server up')
 })
